6eba2b7c1d169abea1e513e3cd8bcd2b140ef0bb
Raviporn Saetiew <raviporn.saetiew@gmail.com>
2018-02-11 20:09:47 +0700

    updated credential file

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 account_sid = 'AC440468d8c135f59f2b7f96f109762d7e'
 auth_token = 'a687432df93f81fa286f815bce3954bd'
-my_cell = '+66909821513'
-my_twilio = '14234019379 '
+my_cell = 'destination phone number that register to TWILIO'
+my_twilio = 'TWILIO phone number'
